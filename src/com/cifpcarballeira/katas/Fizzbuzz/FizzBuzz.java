f2bb8aa5db9581f4d8bf4783c17b8c76a0c222df
Sidro <dam229@ACARBALLEIRA.LOCAL>
2019-02-20 10:13:19 +0100

    1 devuelve 1

---
+++ src/com/cifpcarballeira/katas/Fizzbuzz/FizzBuzz.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cifpcarballeira.katas.Fizzbuzz;
+
+/**
+ *
+ * @author dam229
+ */
+public class FizzBuzz {
+    
+    public String getFizzBuzz(int i){
+        return "1";
+    }
+}
--- test/com/cifpcarballeira/katas/FizzBuzz/Test/TestFizzBuzz.java
+++ test/com/cifpcarballeira/katas/FizzBuzz/Test/TestFizzBuzz.java
@@ -2,6 +2,8 @@ package com.cifpcarballeira.katas.FizzBuzz.Test;
 
 
 
+
+import com.cifpcarballeira.katas.Fizzbuzz.FizzBuzz;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -14,6 +16,11 @@ public class TestFizzBuzz {
     public TestFizzBuzz() {
     }
 
-    // @Test
-    // public void hello() {}
+    @Test
+     public void test1devuelve1() {
+         FizzBuzz fb=new FizzBuzz();
+         String resultado=fb.getFizzBuzz(1);
+         assertEquals("1",resultado);
+         
+     }
 }
