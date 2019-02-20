d8b210ca64ea98b6822d95dd3f03bc2a8308ffc9
Sidro <dam229@ACARBALLEIRA.LOCAL>
2019-02-20 09:54:25 +0100

    Primer commit

---
+++ test/com/cifpcarballeira/katas/FizzBuzz/Test/TestFizzBuzz.java
@@ -0,0 +1,19 @@
+package com.cifpcarballeira.katas.FizzBuzz.Test;
+
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author dam229
+ */
+public class TestFizzBuzz {
+    
+    public TestFizzBuzz() {
+    }
+
+    // @Test
+    // public void hello() {}
+}
